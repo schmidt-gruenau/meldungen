051123e46c0be3c0a8331acccd9a877a29d072fd
Stefan Schmidt <St.Schmidt@online.de>
2022-01-17 14:49:52 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/schmidt-gruenau/meldungen
+
+go 1.16
+
+require github.com/lwb-informatik/strings v1.0.0
---
+++ meldungen.go
@@ -0,0 +1,15 @@
+package meldungen
+// Autor: St. Schmidt
+// Datum: 18.01.2022
+// Zweck: ein Modul mit einem globalen Import
+
+import "fmt"
+import "github.com/lwb-informatik/strings"
+
+func Hallo () {
+	fmt.Println ("*****************************")
+	strings.HalloWelt()
+	fmt.Println ("*****************************")
+}
+
+
